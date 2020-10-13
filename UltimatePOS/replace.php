77a82f19644a8b665ba723d223afea4bae8e0192
Chamara Indrajith <51538345+Chamaraindrajith97@users.noreply.github.com>
2020-10-13 23:53:16 +0530

    Create replace.php

---
+++ UltimatePOS/replace.php
@@ -0,0 +1,16 @@
+
+/resources/views/layouts/partials/header.blade.php (Line 27)
+
+<a class="btn btn-success btn-flat pull-left m-8 hidden-xs btn-sm mt-10" target="popup" onclick="window.open('http://dugut.b-y.me/connecting-to-the-server.php','name','width=600,height=400')">
+<strong><i class="fa fa-upload fa-lg" aria-hidden="true"></i> &nbsp;&nbsp;Upload Data To CSSBIS Cloud Server</strong>
+</a>
+
+
+/resources/views/layouts/partials/header-pos.blade.php (Line 62)
+
+ <a class="btn btn-success btn-flat pull-left m-8 hidden-xs btn-sm mt-10" target="popup" onclick="window.open('http://dugut.b-y.me/connecting-to-the-server.php','name','width=600,height=400')">
+ <strong><i class="fa fa-upload fa-lg" aria-hidden="true"></i> &nbsp;&nbsp;Upload Data To CSSBIS Cloud Server</strong></a>
+ 
+ 
+ 
+ 
